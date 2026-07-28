4f51e50481a826fbfd8bf2094fb3a8cddc8b60a5
Yeswanth Ram <your_github_email@example.com>
2026-07-28 12:52:16 +0530

    cleaned repo

--- check_footer.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('ref/noomo.html', 'utf8');
-
-const footerIdx = html.indexOf('<div class="home-footer">');
-console.log(html.substring(footerIdx, footerIdx + 2500));
